31d8520f7c80f02ab99c2f67a6c5d7d39b95f256
emetsky <emetsky@andrew.cmu.edu>
2018-03-28 22:35:39 -0400

    essential files
    does not include nodejs modules/ settings

---
+++ emote-032818/message_collection.py
@@ -0,0 +1,12 @@
+import json
+import sys
+
+text = sys.argv[1] 
+user = sys.argv[2]
+time = sys.argv[3]
+
+data = json.loads(open("events.json", "r").read())
+data.append({"user": user, "text":text, "time":time})
+
+with open('events.json', 'w') as f:
+    json.dump(data, f)
